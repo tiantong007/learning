572d51e9f1aef323474d1fcc996d3ec37b3aba60
tiantong007 <1511043273@qq.com>
2023-06-22 19:47:44 +0800

    更改结构

--- src/main/java/com/eyy/learning/LearningApplication.java
+++ src/main/java/com/eyy/learning/LearningApplication.java
@@ -1,11 +1,13 @@
-package com.example.learning;
+package com.eyy.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class LearningApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LearningApplication.class, args);
     }
--- src/test/java/com/eyy/learning/LearningApplicationTests.java
+++ src/test/java/com/eyy/learning/LearningApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.learning;
+package com.eyy.learning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
